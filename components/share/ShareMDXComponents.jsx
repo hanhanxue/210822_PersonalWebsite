a552418c143e7d407951408643b65fbdf74bf316
hanhanxue <hanhanxue.hhx@gmail.com>
2021-09-10 18:42:31 -0400

    s

--- components/share/ShareMDXComponents.jsx
+++ components/share/ShareMDXComponents.jsx
@@ -5,7 +5,113 @@ import NextImage from 'next/image'
 let moment = require('moment'); // require
 
 
+const SectionTitle = (props) => {
+    const date = `20${props.date}`
+    return (
+    <section className={`section sectionWhite ${styles.sectionTitle}`}>
+    <div className={`wrapper share`}>
+    <div className={`${styles.letterBox}`}>
+
+            <div className={`title_1 ${styles.titleFrame}`}>
+            {props.title}
+            </div>
+
+            <div className={`subhead ${styles.metaFrame}`}>
+            {`${props.categories[0]} | ${moment(date).format('MMMM Do YYYY')} (${moment(date).fromNow()})`}     
+            </div>
+
+    </div>
+    </div>
+    </section>
+)}
+
+const SectionImage = (props) => (
+    <section className={`section sectionWhite ${styles.sectionImage}`}>
+    <div className={`wrapper share`}>
+    <div className={`${styles.letterBox}`}>
+
+        <div className={`image ${styles.nextImageFrame}`}>
+            <NextImage src={   `/content/share/${props.directory}/${props.image.src}`   } 
+            width={props.image.width} 
+            height={props.image.height}  />
+        </div>
+
+    </div>
+    </div>
+    </section>
+)
+
+const P = (props) => (
+    <section className={`section sectionWhite`}>
+    <div className={`wrapper share`}>
+    <div className={`${styles.letterBox}`}>
+
+        <p {...props} className={`body_2 ${styles.p}`} />
+
+    </div>
+    </div>
+    </section>
+)
+
+const H2 = (props) =>  (
+    <section className={`section sectionWhite`}>
+    <div className={`wrapper share`}>
+    <div className={`${styles.letterBox}`}>
+
+        <h2 {...props} className={`title_3 ${styles.heading} ${styles.h2}`} />
+
+    </div>
+    </div>
+    </section>
+)
+
+const H3 = (props) =>  (
+    <section className={`section sectionWhite`}>
+    <div className={`wrapper share`}>
+    <div className={`${styles.letterBox}`}>
+
+        <h2 {...props} className={`headline ${styles.heading} ${styles.h3}`} />
+
+    </div>
+    </div>
+    </section>
+)
+
+const Ul = (props) =>  (
+    <section className={`section sectionWhite`}>
+    <div className={`wrapper share`}>
+    <div className={`${styles.letterBox}`}>
+
+        <ul {...props} className={`${styles.ul}`} />
+
+    </div>
+    </div>
+    </section>
+)
+
+const Li = (props) =>  (
+    <li {...props} className={`body_2 ${styles.li}`} />
+)
+
+
 const ShareMDXComponents = {
+    SectionTitle: SectionTitle,
+    SectionImage: SectionImage,
+    p: P,
+    h2: H2,
+    h3: H3,
+    ul: Ul,
+    li: Li,
+}
+
+
+
+
+export default ShareMDXComponents
+
+
+
+
 
     // inlineCode: (props) => <code {...props} className={`code ${styles.code}`} />,
 
@@ -25,110 +131,3 @@ const ShareMDXComponents = {
 
     // // <a {...props} className={`${styles.a}`} />
     // },
-
-
-
-
-
-
-    SectionTitle: SectionTitle = (props) => {
-        const date = `20${props.date}`
-        return (
-        <section className={`section sectionWhite ${styles.sectionTitle}`}>
-        <div className={`wrapper share`}>
-        <div className={`${styles.letterBox}`}>
-
-                <div className={`title_1 ${styles.titleFrame}`}>
-                {props.title}
-                </div>
-
-                <div className={`subhead ${styles.metaFrame}`}>
-                {`${props.categories[0]} | ${moment(date).format('MMMM Do YYYY')} (${moment(date).fromNow()})`}     
-                </div>
-
-        </div>
-        </div>
-        </section>
-    )},
-
-    SectionImage: SectionImage = (props) => (
-        <section className={`section sectionWhite ${styles.sectionImage}`}>
-        <div className={`wrapper share`}>
-        <div className={`${styles.letterBox}`}>
-
-            <div className={`image ${styles.nextImageFrame}`}>
-                <NextImage src={   `/content/share/${props.directory}/${props.image.src}`   } 
-                width={props.image.width} 
-                height={props.image.height}  />
-            </div>
-
-        </div>
-        </div>
-        </section>
-    ),
-
-
-
-    p: p = (props) => (
-        <section className={`section sectionWhite`}>
-        <div className={`wrapper share`}>
-        <div className={`${styles.letterBox}`}>
-
-            <p {...props} className={`body_2 ${styles.p}`} />
-
-        </div>
-        </div>
-        </section>
-    ),
-
-    h2: h2 = (props) =>  (
-        <section className={`section sectionWhite`}>
-        <div className={`wrapper share`}>
-        <div className={`${styles.letterBox}`}>
-
-            <h2 {...props} className={`title_3 ${styles.heading} ${styles.h2}`} />
-
-        </div>
-        </div>
-        </section>
-    ),
-
-    h3: h3 = (props) =>  (
-        <section className={`section sectionWhite`}>
-        <div className={`wrapper share`}>
-        <div className={`${styles.letterBox}`}>
-
-            <h2 {...props} className={`headline ${styles.heading} ${styles.h3}`} />
-
-        </div>
-        </div>
-        </section>
-    ),
-            
-
-    ul: ul =(props) =>  (
-        <section className={`section sectionWhite`}>
-        <div className={`wrapper share`}>
-        <div className={`${styles.letterBox}`}>
-
-            <ul {...props} className={`${styles.ul}`} />
-
-        </div>
-        </div>
-        </section>
-    ),
-
-    li: li = (props) =>  (
-        <li {...props} className={`body_2 ${styles.li}`} />
-    ),
-
-
-
-}
-
-
-
-
-export default ShareMDXComponents
-
-
