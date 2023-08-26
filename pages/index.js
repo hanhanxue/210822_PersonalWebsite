654d3d4e7de44d581e2bb66ca823d7cf2b2f4365
hanhanxue <hanhanxue.hhx@gmail.com>
2023-08-26 00:30:48 -0400

    chat button press target

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,8 @@ import moment from 'moment'
 
 const Home = ({ }) => {
 
+
+
     const divStyles = {
         // color: 'blue',
         // lineHeight: 10,
@@ -20,6 +22,9 @@ const Home = ({ }) => {
 
         display: 'flex',
         flexDirection: 'Column',
+
+        maxWidth: '688px',
+        margin: '0 auto',
 
         // display: 'grid',
         // gap: '12px',
@@ -52,22 +57,35 @@ const Home = ({ }) => {
         textDecoration: 'underline',
     }
 
+    const divButtonFrameStyles = {
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'flex-end',
+
+    }
+
+
+
     const buttonStyles = {
+        flex: '0 0 auto',
         // color: 'inherit',
         // border: 'none',
         // padding: 0,
         // font: 'inherit',
         // outline: 'inherit',
+        backgroundColor: 'red',
         
         display: 'flex',
         justifyContent: 'flex-end',
     
         alignItems: 'baseline',
         
+        padding: '16px',
         // padding: '12px 16px',
-        borderRadius: '8px',
-        width: '100%',
-        maxWidth: '640px',
+        // borderRadius: '8px',
+        // width: '100%',
+        minWidth: '280px',
+        
         // margin: 'auto',
         
         background: 'none',
@@ -88,6 +106,10 @@ const Home = ({ }) => {
         // fill: '#4355F9',
         fill: 'black',
 
+    }
+
+    const textPad = {
+        padding: '16px',
     }
 
     const [buttonLabel, setButtonLabel] = useState(`Let's Chat`)
@@ -120,49 +142,55 @@ const Home = ({ }) => {
         <title>Han Han Xue</title>
         {/* <meta name="description" content="Han Han Xue — Interdisciplinary Design" /> */}
       </Head>
-
-      <div  style={divStyles}>
+        <main>
+        <div  style={divStyles}>
 
 
         
 
-      <div  style={divThirdStyles}>
-        <button style={buttonStyles}
-                        onMouseEnter={handleOnMouseEnter}
-                        onMouseLeave={handleOnMouseLeave}
-                        onClick={handleOnClick}>
-                    <h2  className={`  text-brand   `}>{buttonLabel}</h2>
+<div  style={divThirdStyles}>
+    <div style={divButtonFrameStyles}>
+ 
+    <button style={buttonStyles}
+                  onMouseEnter={handleOnMouseEnter}
+                  onMouseLeave={handleOnMouseLeave}
+                  onClick={handleOnClick}>
+              <h2  className={`  text-brand   `} >{buttonLabel}</h2>
 
-                    <svg style={svgStyles}
-                    width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <g clipPath="url(#clip0_523_60)">
-                    <rect x="2" width="12" height="2" />
-                    <rect x="12" width="2" height="12" />
-                    <path fillRule="evenodd" clipRule="evenodd" d="M0 13.9999V12.5858L11.293 1.29285L12.7072 2.70706L1.41424 14H0.000122249L0 13.9999Z" />
-                    </g>
+              <svg style={svgStyles}
+              width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g clipPath="url(#clip0_523_60)">
+              <rect x="2" width="12" height="2" />
+              <rect x="12" width="2" height="12" />
+              <path fillRule="evenodd" clipRule="evenodd" d="M0 13.9999V12.5858L11.293 1.29285L12.7072 2.70706L1.41424 14H0.000122249L0 13.9999Z" />
+              </g>
 
-                    </svg>
-                </button>
-                </div>
-
-
-
-      <div  style={divThirdStyles}>
-      <h1 className={`  text-brand   `}>Design & Product at <NextLink href={`https://www.supermanifold.com/`}>
-                <a style={linkStyles}><u>Super Manifold</u>®</a>
-                </NextLink></h1>
-
-
-
-        </div>
-
-    
-
-    <div  style={divThirdStyles}>
-    
-    <h2  className={`  text-brand   `}>{moment().format('YYYY')}</h2>
+              </svg>
+          </button>
     </div>
-      </div>
+
+          </div>
+
+
+
+<div  style={divThirdStyles}>
+<h1 className={`  text-brand   `}  style={textPad}  >Design & Product at <NextLink href={`https://www.supermanifold.com/`}>
+          <a style={linkStyles}><u>Super Manifold</u>®</a>
+          </NextLink></h1>
+
+
+
+  </div>
+
+
+
+<div  style={divThirdStyles}>
+
+<h2  className={`  text-brand   `}  style={textPad} >{moment().format('YYYY')}</h2>
+</div>
+</div>
+        </main>
+
 
         </>
     )
